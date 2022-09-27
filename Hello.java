fb071ba5a8e2de89994ae88843a719f4572cfc4a
lashwinil <114396106+lashwinil@users.noreply.github.com>
2022-09-27 09:34:00 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -7,6 +7,7 @@ public class Hello{
           System.out.println("Hello world welcome here");
           
           System.out.println("Bye");
+          System.out.println("Bye2");
         }
       }
   }
